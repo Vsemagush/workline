d713303b318f487fd218ec1ea648db790a8f0494
Natalya Gruzdeva <N.A.Gruzdeva@yandex.ru>
2019-11-19 21:49:58 +0400

    изменила на хэш-роутинг

--- src/options/App.js
+++ src/options/App.js
@@ -35,7 +35,7 @@ function App() {
         <Router>
             <div>
                <div>
-                  <Route path="/options" component={Home}/>
+                  <Route exact path="/" component={Home}/>
                   <Route path="/user" component={User} />
                   <Route path="/admin" component={Admin} />
                </div>
