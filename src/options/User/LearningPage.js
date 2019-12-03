4054e6dbaab34648356025764252b87c9d9bbd66
as.zaycev <as.zaycev@tensor.ru>
2019-12-03 11:17:46 +0300

    смена статуса задачи

--- src/options/User/LearningPage.js
+++ src/options/User/LearningPage.js
@@ -9,11 +9,10 @@ import DataBaseApi from '../../storage/db';
 const STATUS_DONE = 'done';
 const STATUS_PROCESSING = 'processing';
 const STATUS_CLOSED = 'closed';
- 
+
 function useGroupedItems(items) {
     return useMemo(() => {
         const result = [];
-        const sortedItems = [];
         if (items && items.length) {
             // Последнее добавленное задание может относиться к первой группе, поэтому сначала группируем
             const groups = new Map();
@@ -31,7 +30,6 @@ function useGroupedItems(items) {
                 itemsInFirstGroup[0].status = STATUS_PROCESSING;
             }
 
-            let index = 0;
             // Актуализируем информацию по статусам групп
             groups.forEach((itemsInTheGroup, key) => {
                 let totalReadyInGroup = 0;
@@ -52,52 +50,28 @@ function useGroupedItems(items) {
                     items: itemsInTheGroup,
                     status: groupStatus,
                 });
-                // Заодно сразу пересортируем (в порядке следования групп) задачи
-                itemsInTheGroup.forEach((item) => {
-                    item.index = index; // Нужен в последующем - брать следующее задания во время смены статуса
-                    index++;
-                    sortedItems.push(item);
-                })
             });
         }
-        return { result, sortedItems };
+        return result;
     }, [items]);
-}
-
-/** Текущее задание */
-function getProcessingItem(items) {
-   const processingItem = items.find(item => {
-      if (item.status === STATUS_PROCESSING) {
-         return item;
-      }
-   });
-
-   return processingItem;
 }
 
 function LearningPage() {
     const [items, setItems] = useState();
-    const res = useGroupedItems(items);
-
-    const groupedTasks = res.result;
-    const sortedItems = res.sortedItems;
-
-    if (JSON.stringify(sortedItems) !== JSON.stringify(items)) {
-        setItems(sortedItems);
-    }
+    const groupedTasks = useGroupedItems(items);
 
     const db = useRef();
 
     // Подключение к БД и загрузка данных
     useEffect(() => {
-        db.current = new DataBaseApi();
+        db.current = new DataBaseApi('test-adminpage');
         db.current.get('tasks').then((result) => {
             const items = db.current.toArray(result);
             db.current.getState().then((result) => { // Загрузка прогресса
                 const progressTasks = db.current.toArray(result);
                 if (progressTasks) {
                     items.forEach((item) => { // Сопоставление прогреса к соответсвующей задаче
-                        let curTask = progressTasks.find(task => task.id === item.id)
+                        let curTask = progressTasks.find(task => task.id === item.id);
                         item.status = curTask && curTask.state;
                     })
                 }
@@ -107,30 +81,44 @@ function LearningPage() {
     }, []);
 
     /** Смена текущего задания для выполнения */
-    const changeProcessingItem = useCallback(
-        () => {
-            const currentProcessingItem = getProcessingItem(items);
-            if (!currentProcessingItem) {
-                return;
-            }
-            const newItems = items.slice();
-            
-            // смена у только что пройденного задания статуса на "Выполнен"
-            db.current.setState(currentProcessingItem.id, STATUS_DONE);
-            let newItem = { ...currentProcessingItem, status: STATUS_DONE };
-            newItems[items.indexOf(currentProcessingItem)] = newItem;
+    const changeProcessingItem = useCallback(() => {
+           const currentProcessingItemIndex = items.findIndex(item => item.status === STATUS_PROCESSING);
+           if (currentProcessingItemIndex === -1) {
+               return;
+           }
+           const newItems = items.slice();
+           const currentProcessingItem = newItems[currentProcessingItemIndex];
+           newItems.splice(currentProcessingItemIndex, 1, {
+               ...currentProcessingItem,
+               status: STATUS_DONE
+           });
+           db.current.setState(currentProcessingItem.id, STATUS_DONE);
 
-            // Если есть еще задания для выполнения, то статус "В процессе" получает следущее задание
-            if (currentProcessingItem.index !== items.length - 1) {
-                const newProcessingItem = items[currentProcessingItem.index + 1];
-                db.current.setState(newProcessingItem.id, STATUS_PROCESSING);
-                newItem = { ...newProcessingItem, status: STATUS_PROCESSING };
-                newItems[items.indexOf(newProcessingItem)] = newItem;
-            }
-            setItems(newItems);
-        },
-        [items]
-    );
+           const group = groupedTasks.find((group) =>
+              group.id === currentProcessingItem.theme
+           );
+           const indexInGroup = group.items.findIndex((item) => item.id === currentProcessingItem.id);
+           let newItem;
+           if (indexInGroup === group.items.length - 1) {
+               const groupIndex = groupedTasks.findIndex((value) => value === group);
+               if (groupIndex !== groupedTasks.length - 1) {
+                  newItem = groupedTasks[groupIndex + 1].items[0];
+               }
+           } else {
+               newItem = group.items[indexInGroup + 1];
+           }
+
+           if (newItem) {
+              const newItemIndex = newItems.findIndex((item) => item === newItem);
+              newItems.splice(newItemIndex, 1, {
+                 ...newItems[newItemIndex],
+                 status: STATUS_PROCESSING
+              });
+              db.current.setState(newItem.id, STATUS_PROCESSING);
+           }
+
+           setItems(newItems);
+       }, [items, groupedTasks]);
 
     return (
         <Pane
@@ -166,7 +154,7 @@ function LearningPage() {
                                         );
                                     })}
                                 </OrderedList>}
-                            
+
                         </Fragment>
                         );
                     })}
