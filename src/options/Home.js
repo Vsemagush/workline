91aaa639fbc4b8ba44fb42f477c55f3579dc616f
SvetaRyzhkova <sveta.ryzhkova.00@bk.ru>
2019-12-06 22:57:26 +0300

    Страница авторизации примитивная

--- src/options/Home.js
+++ src/options/Home.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { TextInput } from 'evergreen-ui';
+import { Button } from 'evergreen-ui';
+import { useHistory } from "react-router-dom";
 
 /** Главная страница - страница авторизации */
 function Home() {
     const EDIT_DEFAULT_LOGIN = 'admin';
     const EDIT_DEFAULT_PASSWORD = '123';
+    const history = useHistory();
+    const login = useRef();
+    const password = useRef();
 
     /** Обработчик нажатия на "Войти" */
-    const onEnterClick = {};
+    const onEnterClick = useCallback(() => {
+        const loginInput = login.current && login.current.value;
+        const passwordInput = password.current && password.current.value;
+        if (loginInput === EDIT_DEFAULT_LOGIN && passwordInput === EDIT_DEFAULT_PASSWORD){
+            history.replace('/admin');
+        }
+        else {
+            history.replace('/user');
+        }
+    }, [history,password,login]);
 
     return (
         <div>
-            
+            <div className="TopBar__header">Workline</div>
+            <div className="TopBar__logoBlock">
+               <img className="TopBar__logo" src="../../assets/logo.png"></img>
+            </div>
+            <div className = "TextBar"><TextInput
+                innerRef={login}
+                placeholder="Логин"
+                className=""
+            />
+            </div>
+            <div className = "TextBar"><TextInput
+                innerRef = {password}
+                placeholder="Пароль"
+            />
+            </div>
+            <div className = "TextBar"><Button 
+                //arginRight={12} 
+                height={40} 
+                onClick={onEnterClick}
+               >
+                Войти
+            </Button>
+            </div>
         </div>
     );
 }
