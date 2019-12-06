97ab22ecf8389f1e364ddecbcd64239654b9bf3f
hp1 <lopatkin_andryushka@mail.ru>
2019-12-06 20:48:02 +0300

    добавил метод для сохранения задачи

--- src/options/Admin/Admin.js
+++ src/options/Admin/Admin.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import TopBar from '../TopBar/TopBar';
+import React, { useRef, useEffect, useCallback } from 'react';
 import { Pane } from 'evergreen-ui';
-
+import DataBase from '../../storage/db'
 function Admin() {
+
+   const data = useRef();
+   useEffect(
+      ()=> {
+         data.current = new DataBase()
+      },[]
+   );
+
+   const saveItem = useCallback(function (item){
+      data.current.updateTask(item.id,item)
+   });
+
    return (
       <Pane background="#DDEBF7">
-         <TopBar caption="Администрирование" />
+         <button onClick={ ()=> saveItem({id:0,description:'1',theme:'2',additional: '3',type: '4',event:'5'})}>
+            Проверка
+         </button>
       </Pane>
    );
 }
