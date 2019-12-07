e728f1aa38d4a326e60e0724ccea70704be23826
hp1 <lopatkin_andryushka@mail.ru>
2019-12-07 08:56:47 +0300

    добавил метод для сохранения группы

--- src/options/Admin/Admin.js
+++ src/options/Admin/Admin.js
@@ -1,35 +1,25 @@
-import React, { useRef, useEffect, useCallback,useState } from 'react';
+import React, { useRef, useEffect, useCallback, useState } from 'react';
 import { Pane } from 'evergreen-ui';
-import DataBase from '../../storage/db'
+import DataBase from '../../storage/db';
+
 function Admin() {
-
    const [taskList, setTaskList] = useState([]);
-
-
    const data = useRef();
-
-
-   const saveItem = useCallback(function (item) {
-      data.current.updateTask(item.id, item)
+   const saveItem = useCallback(function(item) {
+      data.current.updateTask(item.id, item);
    });
-
-
    useEffect(
       () => {
          data.current = new DataBase();
-
          data.current.get('tasks').then(
             (result) => {
                var array = data.current.toArray(result);
                setTaskList(array);
             });
-
-
       }, []);
-
-   const saveGroup = useCallback(function(oldName,newName){
-      for (var i = 0; i < taskList.length; i++){
-         if (taskList[i].theme == oldName) {
+   const saveGroup = useCallback(function(oldName, newName) {
+      for (var i = 0; i < taskList.length; i++) {
+         if (taskList[i].theme === oldName) {
             taskList[i].theme = newName;
          }
       }
@@ -38,20 +28,18 @@ function Admin() {
    return (
       <Pane background="#DDEBF7">
          <ul>
-
             {taskList.map((item) => {
                return (<li key={item.id}>{item.description}</li>);
             })}
          </ul>
-
-         <button onClick={() => saveItem({ id: 0, description: '1', theme: '2', additional: '3', type: '4', event: '5' })}>
+         <button
+            onClick={() => saveItem({ id: 0, description: '1', theme: '2', additional: '3', type: '4', event: '5' })}>
             Проверка
          </button>
-         <button onClick={ ()=> saveGroup('2','55')}>
+         <button onClick={() => saveGroup('2', '55')}>
             проверка_saveGroup
          </button>
       </Pane>
    );
 }
-
 export default Admin;
